eef90a9f1aaaf73806d875dfbbbc0dc85753d780
Luca Micieli <rams23@live.it>
2020-01-11 16:32:44 +0100

    feat: update to angular 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainFromComponent} from './main-from/main-from.component';
+import {MaterialFromComponent} from './material-from/material-from.component';
 
 const routes: Routes = [
   {
     path: 'not-lazy',
     component: MainFromComponent
+  },
+  {
+    path: 'material',
+    component: MaterialFromComponent
   },
   {
     path: 'lazy',
